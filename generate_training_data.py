bf9f2e59adc6b4974189ac837b49a9cde88eac20
Xuan-Loc Nguyen <springbud@gmail.com>
2021-04-16 11:59:08 +0800

    Update generate_training_data.py
    add display

--- generate_training_data.py
+++ generate_training_data.py
@@ -1,5 +1,6 @@
 from py3dbp import Packer, Bin, Item
-import open3d as open3d
+import numpy as np
+import open3d as o3d
 import random
 
 class Online3DPackingDataGenerator:
@@ -26,30 +27,57 @@ class Online3DPackingDataGenerator:
         index=0
         while (current_volume<bin_volume): 
             index+=1
-            item_W =random.randint(1,20)#random.uniform(self.item_W_range[0],self.item_W_range[1])
-            item_H =random.randint(1,20)#random.uniform(self.item_H_range[0],self.item_H_range[1])
-            item_L =random.randint(1,20)#random.uniform(self.item_L_range[0],self.item_L_range[1])
+            item_W =random.randint(10,20)#random.uniform(self.item_W_range[0],self.item_W_range[1])
+            item_H =random.randint(10,20)#random.uniform(self.item_H_range[0],self.item_H_range[1])
+            item_L =random.randint(10,20)#random.uniform(self.item_L_range[0],self.item_L_range[1])
             current_volume+=item_W*item_H*item_L
             self.packer.add_item(Item('Item '+str(index), item_W, item_H, item_L, 0))
         
         self.packer.pack()
-        
+
+        item_list=[]
         for b in self.packer.bins:
             print(":::::::::::", b.string())
-
+            mesh_bin = self.create_mesh_box(width = b.width, height = b.height, depth = b.depth)#, dx=b.position[0], dy=b.position[1], dz=b.position[2])#position
+            mesh_bin.compute_vertex_normals()
+            mesh_bin.paint_uniform_color([random.uniform(0,1), random.uniform(0,1), random.uniform(0,1)])
+            lineset_bin=o3d.geometry.LineSet.create_from_triangle_mesh(mesh_bin)
+            item_list.append(lineset_bin)
             print("FITTED ITEMS:")
             for item in b.items:
                 print("====> ", item.string())
-
+                mesh_box = self.create_mesh_box(width = item.width, height = item.height, depth = item.depth, dx=item.position[0], dy=item.position[1], dz=item.position[2])#position
+                mesh_box.compute_vertex_normals()
+                mesh_box.paint_uniform_color([random.uniform(0,1), random.uniform(0,1), random.uniform(0,1)])
+                item_list.append(mesh_box)
             print("UNFITTED ITEMS:")
             for item in b.unfitted_items:
                 print("====> ", item.string())
 
             print("***************************************************")
             print("***************************************************")
+        
+        o3d.visualization.draw_geometries(item_list)
 
-
-
+    def create_mesh_box(self,width, height, depth, dx=0, dy=0, dz=0):
+        box = o3d.geometry.TriangleMesh()
+        vertices = np.array([[0,0,0],
+                            [width,0,0],
+                            [0,0,depth],
+                            [width,0,depth],
+                            [0,height,0],
+                            [width,height,0],
+                            [0,height,depth],
+                            [width,height,depth]])
+        vertices[:,0] += dx
+        vertices[:,1] += dy
+        vertices[:,2] += dz
+        triangles = np.array([[4,7,5],[4,6,7],[0,2,4],[2,6,4],
+                            [0,1,2],[1,3,2],[1,5,7],[1,7,3],
+                            [2,3,7],[2,7,6],[0,4,1],[1,4,5]])
+        box.vertices = o3d.utility.Vector3dVector(vertices)
+        box.triangles = o3d.utility.Vector3iVector(triangles)
+        return box
     def generate_data_test(sefl):
 
         packer = Packer()
